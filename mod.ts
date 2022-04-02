0806b7da54ce7b4d950cca3c76a42ee428dbdc74
Hiroki <1127785+azulamb@users.noreply.github.com>
2022-04-03 08:50:51 +0900

    Fix cookie.

--- mod.ts
+++ mod.ts
@@ -7,7 +7,7 @@ import { dirname, fromFileUrl, join, VERSION } from './src/denostd.ts';
 export const STD_VERSION = VERSION;
 export { Server } from './src/server.ts';
 export { Middlewares } from './src/middleware.ts';
-export { getCookies, setCookie } from './src/denostd.ts';
+export { Cookie } from './src/denostd.ts';
 
 export function createAbsolutePath(meta: ImportMeta, path = '') {
 	return join(dirname(fromFileUrl(meta.url)), path);
