8ea1a7b0178b748e0d263437a6242e68f37ebcaf
Hiroki <1127785+azulamb@users.noreply.github.com>
2022-04-03 09:09:43 +0900

    Fix format.

--- src/denostd.ts
+++ src/denostd.ts
@@ -1,6 +1,6 @@
 export const VERSION = '0.133.0';
 export * from 'https://deno.land/std@0.133.0/http/server.ts';
 export { dirname, extname, fromFileUrl, join } from 'https://deno.land/std@0.133.0/path/mod.ts';
-import { getCookies, setCookie, deleteCookie } from 'https://deno.land/std@0.133.0/http/cookie.ts';
+import { deleteCookie, getCookies, setCookie } from 'https://deno.land/std@0.133.0/http/cookie.ts';
 export const Cookie = { get: getCookies, set: setCookie, delete: deleteCookie };
 export { Status, STATUS_TEXT } from 'https://deno.land/std@0.133.0/http/http_status.ts';
